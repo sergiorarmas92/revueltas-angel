ba978080b14e86e545a73acad4ebcc10bc95d3f1
Sergio Reyes <sergio.ra38@gmail.com>
2020-04-23 21:26:52 -0500

    home and tabs created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def about
+    end
+
+    def educacion
+    end
+
+    def radio
+    end
+
+    def servicios
+    end
+
+end
