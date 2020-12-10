d5e98f900c5e1f34f9cd6450c82d06986714ccaa
Sergio Reyes <sergio.ra38@gmail.com>
2020-12-09 19:06:59 -0600

    new links added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,4 +17,58 @@ class PagesController < ApplicationController
     def revolturas
     end
 
+    def filosofiaparaninos
+    end
+
+    def planetaneta
+    end
+
+    def elmisteriodelaspreguntassinrespuesta
+    end
+
+    def comosaberquenoeresunrobot
+    end
+
+    def robotropolis
+    end
+
+    def habitarelarte
+    end
+
+    def esculpireltiempo
+    end
+
+    def remix
+    end
+
+    def prohibidopensar
+    end
+
+    def elritualdelamemoria
+    end
+
+    def chicasnoches
+    end
+
+    def elrumordeladiscordia
+    end
+
+    def radiorevueltas
+    end
+
+    def pedagogias
+    end
+
+    def identidadverbal
+    end
+
+    def estrategiaverbal
+    end
+
+    def contenidoverbal
+    end
+
+
+
+
 end
